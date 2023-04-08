67772b306d7f7ffcd6f4552d89f5ab59c62224d0
G1orian <g1orian@proton.me>
2023-04-08 16:10:31 +0700

    oz lib

--- src/main/Clonable.sol
+++ src/main/Clonable.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "../openzeppelin/access/Ownable.sol";
-import "../openzeppelin/proxy/Clones.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/proxy/Clones.sol";
 import "../interfaces/IClonable.sol";
 
 // @title Base Clonable Contract
--- src/main/PrivateVaultBase.sol
+++ src/main/PrivateVaultBase.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "../openzeppelin/token/ERC20/extensions/ERC4626.sol";
-import "../openzeppelin/access/Ownable.sol";
-import "../openzeppelin/proxy/Clones.sol";
-import "../openzeppelin/token/ERC20/utils/SafeERC20.sol";
-import "../openzeppelin/token/ERC721/IERC721.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/proxy/Clones.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 import "./Clonable.sol";
 
 // @title Base Private Vault Contract
